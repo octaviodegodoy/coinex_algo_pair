690fc0c0075db846521af74e60a3435e4989c17e
octaviodegodoy <octaviodegodoy@gmail.com>
2025-08-18 13:01:15 -0300

    included half life check

--- AlgoTradingFT.py
+++ AlgoTradingFT.py
@@ -5,16 +5,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from filterpy.kalman import KalmanFilter
 from statsmodels.tsa.stattools import adfuller
+from sklearn.linear_model import LinearRegression
 
-API_KEY = "key"
+API_KEY = ""
 SECRET = "secret"
 
 BASE_URL = "https://api.coinex.com"
-with open('C:/Users/degod/Dev/python-coinex/key_data.txt', 'r') as file:
-         api_key = file.read().strip()
-         secret = file.read().strip()
-         print(f" Key is {api_key} and secret is {secret}")
-
+#with open('C:/Users/degod/Dev/python-coinex/key_data.txt', 'r') as file:
+         #api_key = file.read().strip()
+         #secret = file.read().strip()
+         #print(f" Key is {api_key} and secret is {secret}")
+#         print("teste")
 
 API_KLINE_PATH = "/v2/futures/kline"
 
@@ -123,6 +124,23 @@ def check_cointegration(symbolY,symbolX,periods):
 
     # Convert `dynamic_spread` to a pandas Series
     spread_series = pd.Series(zscore_spread, name="dynamic_spread")
+    # Calculate the lagged spread and the difference
+    spread_lagged = spread_series.shift(1).fillna(0)  # Lagged spread (y_{t-1})
+    spread_delta = spread_series - spread_lagged  # Change in spread (Delta y_t)
+    
+    # Reshape the data for linear regression
+    X = spread_lagged.values.reshape(-1, 1)  # Independent variable
+    y = spread_delta.values  # Dependent variable
+
+    # Perform linear regression: y = kappa * X + noise
+    model = LinearRegression(fit_intercept=False)
+    model.fit(X, y)
+    kappa = -model.coef_[0]  # Speed of mean reversion (kappa)
+
+    # Calculate the half-life
+    half_life = np.log(2) / kappa
+
+
     adf_result = adfuller(spreads)
     p_value = adf_result[1]
     coint_t = adf_result[0]
@@ -130,9 +148,7 @@ def check_cointegration(symbolY,symbolX,periods):
     t_check = coint_t < critical_value
     coint_flag = p_value < 0.05 and t_check
 
-
-    print(f" Assets correlation is {correlation} spreads is {z_scores_rolling[-1]}")
-    return z_scores_rolling,coint_flag 
+    return z_scores_rolling,coint_flag,correlation,half_life 
      
 
 def main():
@@ -157,8 +173,13 @@ def plot_series(price_data,symbol_y,symbol_x):
     price_data['Return1'] = price_data['Price1'].pct_change().cumsum()
     price_data['Return2'] = price_data['Price2'].pct_change().cumsum()
 
-    z_scores_rolling,coint = check_cointegration(symbol_y,symbol_x,252)
-    print(f"Is cointegrated ? {coint}")
+    z_scores_rolling,coint,correlation,half_life = check_cointegration(symbol_y,symbol_x,252)
+    
+    if coint and half_life < 3:
+        print(f"Is cointegrated ? {coint} half life is {half_life} z score is {z_scores_rolling[-1]} correlation is {correlation}")
+        plot_asset_spreads(price_data,z_scores_rolling,symbol_y,symbol_x)
+
+def plot_asset_spreads(price_data,z_scores,symbol_y,symbol_x):
 
     # Plot
     plt.figure(figsize=(10, 5))
@@ -172,9 +193,9 @@ def plot_series(price_data,symbol_y,symbol_x):
     plt.xticks(rotation=45)
     plt.tight_layout()
 
-           # Plot the original and smoothed Z-scores
+    # Plot the original and smoothed Z-scores
     plt.subplot(2, 1, 2)
-    plt.plot(price_data.index,z_scores_rolling, label='Rolling Z-scores', color='green', linewidth=1)
+    plt.plot(price_data.index,z_scores, label='Rolling Z-scores', color='green', linewidth=1)
     plt.axhline(0, color='black')
     plt.axhline(1, color='blue', linestyle='--')
     plt.axhline(2, color='green', linestyle='--', label='+2 Std Dev')
@@ -185,8 +206,7 @@ def plot_series(price_data,symbol_y,symbol_x):
     plt.title(f'Z-scores for assets {symbol_y} and {symbol_x} ')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.ylim(-4.0, 4.0)
-    if coint:
-        plt.show()    
+    plt.show()    
 
 if __name__ == "__main__":
     main()
